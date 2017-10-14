698a809ee1c15a6f8e25bb889d89633859587727
Wade (Kuo-Wei, Lee) <kxl162230@utdallas.edu>
2017-10-13 22:16:52 -0500

    Wade solving-sol #056, #087

--- 087/Wade #087/script.js
+++ 087/Wade #087/script.js
@@ -1,11 +1,11 @@
 // Code goes here
+var canvasWidth = 401;
+var canvasHeight = 401;
 var rectX = 0;
 var rectY = 0;
 var rectWidth = 400;
-
-var canvasWidth = 401;
-var canvasHeight = 401;
 var lineSpace = 10;
+
 
 
 function setup() {
@@ -16,14 +16,15 @@ function setup() {
 
 function draw() {
 
-  fill('white');
-  rect(rectX, rectY, rectWidth, rectWidth);
+
   //V
-  for (var i = 0; i < rectWidth; i = i + lineSpace) {
+  for (var i = 0; i <= rectWidth; i = i + lineSpace) {
+    stroke('green');
     line(i + rectX, rectY, i + rectX, rectY + rectWidth)
   }
   //H
   for (i = rectY; i < rectY + rectWidth; i = i + lineSpace) {
+    stroke('yellow');
     if (i < rectY + rectWidth / 2) {
       line(rectX + rectWidth / 2, i, rectX + rectWidth, i)
     } else {
@@ -32,11 +33,13 @@ function draw() {
   }
   //DR
   for (i = 0; i < rectWidth/2; i = i + lineSpace) {
+    stroke('blue');
     line(rectWidth/2+i,rectWidth/2,rectWidth,rectWidth-i);
     line(rectWidth/2,rectWidth/2+i,rectWidth-i,rectWidth);
   }
   //DL
   for (i = 0; i < rectWidth*2; i = i + lineSpace) {
+    stroke('red')
     line(rectX,rectY+rectWidth/2+i+lineSpace,rectX+i+lineSpace,rectY+rectWidth/2)
   }
 
